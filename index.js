d89903735a7367edfad7cb74c9f2fd384e25ca88
gyhudicjhbgyr <jsjcamesking766@hotmail.com>
2024-05-09 18:59:00 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function canJump(nums) {
-  let lastPos = nums.length - 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (i + nums[i] >= lastPos) lastPos = i;
+function levelOrderBottom(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.unshift(currentLevel);
   }
-  return lastPos === 0;
+  return result;
 }
